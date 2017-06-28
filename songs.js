38db4958000e106529d150bdf6cad8c4ef324f07
Megan B <brown.m.j@outlook.com>
2017-06-28 08:48:14 -0500

    loop through songs and inject them into list music DOM

--- songs.js
+++ songs.js
@@ -1,11 +1,4 @@
 let songs = [];
-
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-
 let songListOutput = document.getElementById("song-list");
 let listMusicLink = document.getElementById("list-music-link");
 let addMusicLink = document.getElementById("add-music-link");
@@ -16,41 +9,21 @@ let newAlbumInput = document.getElementById("album-input");
 let listMusicPage = document.getElementById("list-music-page");
 let addMusicPage = document.getElementById("add-music-page");
 
-
-for (i=0; i<songs.length; i++) {
-	songs[i] = songs[i].replace(/\*|@|\(|!/g, '');
-	songs[i] = songs[i].replace(/>/g, "-")
-}
-
-function addInitialDom() {
-	for (i=0; i<songs.length; i++) {
-		songListOutput.innerHTML += `<div class="song-item">${songs[i]}</div>`;
-	}
-}
-
-function songsToDom() {
-	let songCard = document.createElement("div");
-	songCard.setAttribute("class", "song-item");
-	for (i=0; i<songs.length; i++) {
-		songCard.innerHTML = songs[i];
-	}
-	songListOutput.appendChild(songCard);
-}
-
 function newSongToArray() {
 	let newSong = newSongInput.value;
 	let newArtist = newArtistInput.value;
 	let newAlbum = newAlbumInput.value;
-	songs.push(`${newSong} - by ${newArtist} on the album ${newAlbum}`);
+	songs.push(`<h2>${newSong}</h2>
+							 <ul>
+							 <li>${newArtist}</li>
+							 <li>${newAlbum}</li>
+							 </ul>`)
 }
 
-window.addEventListener("load", function() {
-	addInitialDom()
-})
 
 addButton.addEventListener("click", function() {
 	newSongToArray();
-	songsToDom();
+	console.log(songs)
 })
 
 addMusicLink.addEventListener("click", function() {
@@ -63,20 +36,46 @@ listMusicLink.addEventListener("click", function() {
 	addMusicPage.classList.add("hidden");
 })
 
+function songs1ToDom(songs1Arr) {
+	songs.forEach( function(currentSong) {
+		songListOutput.innerHTML += `<h2>${currentSong.name}</h2>
+																 <ul>
+																 <li>${currentSong.artist}</li>
+																 <li>${currentSong.album}</li>
+																 </ul>`
+	});
+}
+function songs2ToDom(songs2Arr) {
+	songs.forEach( function(currentSong) {
+		songListOutput.innerHTML += `<h2>${currentSong.name}</h2>
+																 <ul>
+																 <li>${currentSong.artist}</li>
+																 <li>${currentSong.album}</li>
+																 </ul>`
+	});
+}
 
+function loadSongs1(callback) {
+	let load = new XMLHttpRequest();
+	load.addEventListener("load", function() {
+		songs = JSON.parse(this.responseText).songs1;
+		callback(songs)
 
+	})
+	load.open("GET", "data/songs1.json");
+	load.send();
+}
 
+function loadSongs2(callback) {
+	let load = new XMLHttpRequest();
+	load.addEventListener("load", function() {
+		songs = JSON.parse(this.responseText).songs2;
+		callback(songs)
 
+	})
+	load.open("GET", "data/songs2.json");
+	load.send();
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+loadSongs1(songs1ToDom);
+loadSongs2(songs2ToDom);
